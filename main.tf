e3688e5e6a0756169ea1d6aa657ccaca75ae2a8e
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-20 22:42:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "aurora_cluster" {
+  source = "./modules/aurora"
+
+  cluster_identifier = "example-aurora-cluster"
+  engine             = "aurora-mysql"
+  engine_version     = "5.7.mysql_aurora.2.11.1"
+  instance_class     = "db.r6g.large"
+  db_name            = "exampledb"
+  username           = "auroraadmin"
+  secret_name        = "aurora-db-credentials1"
+  
+}
