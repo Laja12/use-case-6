053557437d8593cb06fa57154c05eae3555ee518
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-20 22:40:52 +0530

    Create var.tf

---
+++ modules/aurora/var.tf
@@ -0,0 +1,8 @@
+variable "cluster_identifier" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "db_name" {}
+variable "username" {}
+variable "secret_name" {}
+
